c930125d3a2e989041d2a7006d0ba5f7acaa6f61
Md Ataullah <ataullahm100@gmail.com>
2025-02-13 00:36:36 +0600

    Update order model to reference Product in product field

--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -4,7 +4,7 @@ import { IOrder } from "./order.interface";
 const orderSchema = new Schema<IOrder>(
   {
     email: { type: String, required: true },
-    product: { type: Schema.Types.ObjectId, required: true },
+    product: { type: Schema.Types.ObjectId, ref: "Product", required: true },
     quantity: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
   },
